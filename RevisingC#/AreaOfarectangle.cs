de5fc3c18f5e3ac5faffb057cd8da0aac1d45f27
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-07 23:38:53 +0530

    Some more problems

---
+++ RevisingC#/AreaOfarectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class AreaOfarectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle();
+        }
+
+        static void Rectangle()
+        {
+            // area of rectangle = side * side;
+            double side = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Area of rectangle."+side*side);
+
+           
+        }
+    }
+}
---
+++ RevisingC#/SwapTwoNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class SwapTwoNumbers
+    {
+        static void Main(string[] args)
+        {
+            //SwappingNumber();
+            Swappingthenumberwithoutusingthirdvariable();
+        }
+
+        static void SwappingNumber()
+        {
+            Console.WriteLine("please enter first number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swapping the values.."+first+",,"+second);
+
+            int third = second;
+            second = first;
+            first = third;
+
+            Console.WriteLine("After swapping the values.."+first+",,"+second);
+        }
+
+        static void Swappingthenumberwithoutusingthirdvariable()
+        {
+            Console.WriteLine("please enter first number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swapping the values.." + first + ",," + second);
+
+            first = first + second;
+            second = first - second;
+            first = first - second;
+
+            Console.WriteLine("After swapping the values.." + first + ",," + second);
+        }
+    }
+}
