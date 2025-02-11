5f534e9ac6b038bddabb69c96ffdf1c11ab10bc4
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-11 12:12:53 +0530

    Added couple of basic codes

---
+++ RevisingC#/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            Fact();
+        }
+
+        static void Fact()
+        {
+
+            Console.WriteLine("Please enter the number");
+            int fact = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+           
+
+            for (int i = fact; i > 0; i--)
+            {
+                result = result * i;
+            }
+
+            Console.WriteLine("Total-value.."+result );
+
+        }
+    }
+}
---
+++ RevisingC#/LCM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class LCM
+    {
+        static void Main(string[] args)
+        {
+            Findlcm();
+        }
+
+        public static void Findlcm() // brute-force-approach
+        {
+            Console.WriteLine("Enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+           int c = Math.Max(a,b);
+
+            while (true)
+            {
+                if (c % a == 0 && c % b == 0)
+                {
+                    break;
+                }
+                c++;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
