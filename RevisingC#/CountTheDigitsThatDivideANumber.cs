1e762fd95419a9809dc619f452c84d68f125ee42
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-20 20:32:49 +0530

    leetcode problem uploaded

---
+++ RevisingC#/CountTheDigitsThatDivideANumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class CountTheDigitsThatDivideANumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Counts(1248));
+        }
+
+        public static int Counts(int num)
+        {
+
+            int temp = num;
+            int count = 0;
+            int digit;
+            while (temp > 0)
+            {
+                digit = temp % 10;
+                temp = temp / 10;
+
+                if (num % digit == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
