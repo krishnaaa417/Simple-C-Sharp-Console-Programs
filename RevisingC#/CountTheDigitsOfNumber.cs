e6cca31782695e6f27b5aa36e8fd349d09a40333
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-21 18:22:17 +0530

    one more problem added

---
+++ RevisingC#/CountTheDigitsOfNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class CountTheDigitsOfNumber
+    {
+        static void Main(string[] args)
+        {
+            Digits();
+        }
+
+        static void Digits()
+        {
+            int n = 91214;
+            int temp = 0;
+             while (n > 0)
+            {
+                int result = n % 10;
+               temp++;
+                n = n / 10;
+            }
+            Console.WriteLine(temp);
+        }
+    }
+}
