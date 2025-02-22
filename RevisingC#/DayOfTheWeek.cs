b6a9a1c007fef3b1aed7eed2170c0cb434098cc9
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-22 14:53:17 +0530

    Leetcode 1185 added

---
+++ RevisingC#/DayOfTheWeek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class DayOfTheWeek
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( DayOfftheWeek());
+        }
+
+        public static string DayOfftheWeek()
+        {
+            int day = Convert.ToInt32(Console.ReadLine());
+            int month = Convert.ToInt32(Console.ReadLine());
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            var datetime = new DateTime(year, month, day);
+            return datetime.DayOfWeek.ToString();
+
+        }
+    }
+}
