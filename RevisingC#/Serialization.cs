a62ab898cb93b8c5ca64dbbaf64bdc243cf31ca8
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-23 20:35:13 +0530

    Serialization

---
+++ RevisingC#/Serialization.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class Serialization
+    {
+        static void Main(string[] args)
+        {
+            Emp emp = new Emp();
+            emp.Id = 1;
+            emp.Name = "krishna";
+            //convert object into json
+            string json = JsonConvert.SerializeObject(emp);
+            Console.WriteLine(json);
+        }
+    }
+
+    public class Emp
+    {
+        public int Id;
+        public string Name;
+    }
+}
