05fb17ce69ef1e4cfcc88e0bed7b6a8e4e6408aa
krishnaaa210 <krishnaaa210@gmail.com>
2025-02-26 22:47:49 +0530

    Added one more problem

---
+++ RevisingC#/PerfectSquarenumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class PerfectSquarenumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perfect-SquareNumber");
+            int num = 16;
+           for (int i = 1; i * i <= num; i++)
+            {
+                if(i * i == num)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+          
+        }
+    }
+}
