f141faf037d868b3902234209a5a24036bf0ac9b
krishnaaa210 <krishnaaa210@gmail.com>
2025-03-06 21:28:11 +0530

    DelegatesRemoveDuplicateThroughLinq

---
+++ RevisingC#/Delegates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+   public delegate void Dele(int x, int y);
+    internal class Delegates
+    {
+        static void Main(string[] args)
+        {
+            Dele d = new Dele(Add);
+            d = d + Mul;
+            d(20, 30);
+            //d1(20,30);
+        }
+
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a*b);
+        }
+    }
+}
---
+++ RevisingC#/RemoveDuplicateThroughLinq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class RemoveDuplicateThroughLinq
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 4, 34, 2, 2, 1, 6, 22, 102 };
+
+            List<int> numbers = list.Distinct().ToList();
+
+            Console.WriteLine("Unique numbers." + string.Join(",",numbers));
+        }
+    }
+}
