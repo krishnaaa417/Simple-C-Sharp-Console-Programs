96392e8bad6db947f775603f97bed2b9f294d69f
krishnaaa210 <krishnaaa210@gmail.com>
2025-03-14 20:13:29 +0530

    Interview Question

---
+++ RevisingC#/ReverseTheNumberInList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingC_
+{
+    internal class ReverseTheNumberInList
+    {
+        static void Main(string[] args)
+        {
+            List<int> reverseNumber = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+             reverseNumber.Reverse();
+            foreach (int i in reverseNumber)
+            {
+                Console.Write(i+",");
+            }
+        }
+    }
+}
